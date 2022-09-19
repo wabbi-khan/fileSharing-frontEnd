36a458db902ceb131ff6da4f21cf39c44a38573f
wabbi-khan <kwahab789@gmail.com>
2022-09-19 14:22:05 +0500

    ok

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const dropZone = document.querySelector(".drop-zone");
 // ? jabh bhi drag hoga tw ye chlega
 
 dropZone.addEventListener("dragover", (e) => {
+    e.preventDefault();
     if (!dropZone.classList.contains("dragged")) {
         dropZone.classList.add("dragged");
     }
@@ -11,5 +12,12 @@ dropZone.addEventListener("dragover", (e) => {
 // ? jabh bhi drag hatega hoga tw ye chlega
 
 dropZone.addEventListener("dragleave", (e) => {
+    dropZone.classList.remove("dragged");
+});
+
+// ? jabh bhi drop ho jayga tw ye chlega
+
+dropZone.addEventListener("drop", (e) => {
+    e.preventDefault();
     dropZone.classList.remove("dragged");
 });
